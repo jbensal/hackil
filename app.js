91b705b1ba90ba56919dc40fb7c28033dd838713
chrischris292 <cbchan2@illinois.edu>
2015-02-28 04:08:49 -0600

    insta

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
 var express = require('express');
 var Factual = require('factual-api');
-var factual = new Factual('FACTUAL_API_KEY', 'FACTUAL_SECRET')
 var app = express();
+var api = require('instagram-node').instagram();
+
 
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
-console.log("Express server running on " + port);
-console.log('hi');
+var request = require('request');
 
-app.get('/test', function(req, res){
-	res.send('Hello, world');
-});
 
 app.listen(process.env.PORT || port);
+
+
+api.use({ client_id: 'b61282d995b742f1b640cdbd5409ecd7',
+         client_secret: '914640947582426aaf675a742b49dec5' });
+console.log("ig")
+var redirect_uri = 'http://localhost:3000/handleauth';
+ 
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+ 
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send(err);
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      res.send('You made it!!');
+        request
+  .get('https://api.instagram.com/v1/users/self/feed')
+  .on('response', function(error, response, body) {
+    console.log("body" + body) // 200 
+  })
+    }
+  });
+};
+
+//test function
+exports.test = function(req,res){
+	instaSearch(36.841557383,-76.135525865)
+}
+
+function instaSearch(latitude,longitude){
+	api.location_search({ lat: latitude, lng: longitude }, function(err, result, remaining, limit) {
+	console.log(result[0].id.toString())
+
+	api.location_media_recent(result[0].id.toString(), function(err, result, pagination, remaining, limit) {
+	console.log(result)
+	});
+
+});
+
+}
+
+
+// This is where you would initially send users to authorize 
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI 
+app.get('/handleauth', exports.handleauth);
+app.get('/instaSearch', exports.test);
+
+
