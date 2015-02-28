3b3ef7ae40c4c012fd2f45f475f797f468404a4b
Jay Bensal <jay.bensal@gmail.com>
2015-02-28 12:37:36 +0000

    adding in factual query

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var Factual = require('factual-api');
+=======
+var factual = new Factual('', '')
 var app = express();
 var api = require('instagram-node').instagram();
 
 
 app.use(express.static(__dirname + '/public'));
 
+<<<<<<< Updated upstream
 var port = process.env.PORT || 3000;
 var request = require('request');
 
 app.listen(process.env.PORT || port);
 
-//Instagram Features: 
-
+//Instagram Features:
 api.use({ client_id: 'b61282d995b742f1b640cdbd5409ecd7',
          client_secret: '914640947582426aaf675a742b49dec5' });
 console.log("ig")
@@ -51,11 +53,85 @@ function instaSearch(latitude,longitude){
 
 }
 
-
 // This is where you would initially send users to authorize 
 app.get('/authorize_user', exports.authorize_user);
 // This is your redirect URI 
 app.get('/handleauth', exports.handleauth);
 app.get('/instaSearch', exports.test);
 
+var port = process.env.PORT || 8000;
+console.log("Express server running on " + port);
 
+// factual.get('/t/places-us', {q:"starbucks", filters:{"$or":[{"locality":{"$eq":"los angeles"}},{"locality":{"$eq":"santa monica"}}]}}, function (error, res) {
+//   console.log(res.data);
+// });
+
+// factual.get('/t/places-us/schema', function (error, res) {
+//   console.log(res.view);
+// });
+
+app.get('/', function(req, res){
+	console.log('request received on index')
+	res.send('homepage');
+});
+
+//Search route. Uses query parameters to pass in values into the Factual search.
+app.get('/search', function(req, res) {
+  //latitude
+  var latitude = req.query.lat
+  console.log(typeof(+longitude));
+  console.log("Latitude " + latitude)
+
+  //longitude
+  var longitude = req.query.lon
+  console.log("Longitude " + longitude)
+  
+  //locality? country?
+
+  // categories
+  var cat_array = req.query.categories.split(',');
+  var category_ids = new Array();
+
+  for (var i = 0; i < cat_array.length; i++){
+  	if (cat_array[i] == "auto"){
+  		category_ids.push(2);
+  	}
+  	else if (cat_array[i] == "gov"){
+  		category_ids.push(20);
+  	}
+  	else if (cat_array[i] == "health"){
+  		category_ids.push(62);
+  	}
+  	else if (cat_array[i] == "landmark"){
+  		category_ids.push(107);
+  	}
+  	else if (cat_array[i] == "retail"){
+  		category_ids.push(123);
+  	}
+  	else if (cat_array[i] == "biz"){
+  		category_ids.push(177);
+  	}
+  	else if (cat_array[i] == "social"){
+  		category_ids.push(308);
+  	}
+  	else if (cat_array[i] == "sports"){
+  		category_ids.push(372);
+  	}
+  	else if (cat_array[i] == "transport"){
+  		category_ids.push(415);
+  	}
+  	else if (cat_array[i] == "travel"){
+  		category_ids.push(430);
+  	}
+  }
+
+  //Note that latitude and longitude must have 6 digits.
+  factual.get('/t/places-us', {filters:{category_ids:{"$includes_any":category_ids}}, geo:{"$circle":{"$center":[+latitude, +longitude],"$meters":1000}}}, function(req, res){
+  	console.log(res.data);
+
+  })
+
+  res.send('hi');
+});
+
+app.listen(process.env.PORT || port);
