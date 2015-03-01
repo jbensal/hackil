c099a2d416db07526c742aadd0eb1c6cc57deaad
William Xie <wxie6@illinois.edu>
2015-02-28 21:51:13 -0600

    Merged new app.js functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var Factual = require('factual-api');
+
 var factual = new Factual('onyxZs8ek9NvAOgX9NrqBtCwGXkZoRcgqnJVPmnN', 'yPIF4i2HaITiPy1C9DmHNG4xHSVXAnxlH2GgrFyP')
 var yelp = require("yelp").createClient({
   consumer_key: "qydJIFh5gHAyMj6KlrNmtw", 
@@ -8,13 +10,11 @@ var yelp = require("yelp").createClient({
   token_secret: "v9RYnoJBG6SMcuJFFUjQP5Nm30o"
 });
 
-
 var app = express();
 var api = require('instagram-node').instagram();
 var request = require('request');
 var engines = require('consolidate');
-var app = express();
-
+// var app = express(); // declare twice needeD?
 
 app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 3000;
@@ -28,11 +28,10 @@ console.log("Express server running on " + port);
 //Instagram Features:
 api.use({ client_id: 'b61282d995b742f1b640cdbd5409ecd7',
          client_secret: '914640947582426aaf675a742b49dec5' });
-console.log("ig")
 var redirect_uri = 'http://localhost:3000/handleauth';
  
 exports.authorize_user = function(req, res) {
-  redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
 };
  
 exports.handleauth = function(req, res) {
@@ -43,11 +42,10 @@ exports.handleauth = function(req, res) {
     } else {
       console.log('Yay! Access token is ' + result.access_token);
       res.send('You made it!!');
-      	api.user_followers("self", function(err, users, pagination, remaining, limit) {
-		console.log(users)
-		console.log(err)
-	});
-
+      api.user_followers("self", function(err, users, pagination, remaining, limit) {
+	      console.log(users);
+	      console.log(err);
+      });
     }
   });
 };
@@ -58,16 +56,90 @@ exports.test = function(req,res){
 }
 
 function instaSearch(latitude,longitude){
-	api.location_search({ lat: latitude, lng: longitude, distance: 5000}, function(err, result, remaining, limit) {
-	console.log(result[0].id.toString())
-
-	api.location_media_recent(result[0].id.toString(), function(err, result, pagination, remaining, limit) {
-	 console.log(result)
-	});
-
-});
-
+  api.location_search({ lat: latitude, lng: longitude, distance: 5000}, function(err, result, remaining, limit) {
+    console.log(result[0].id.toString())
+    api.location_media_recent(result[0].id.toString(), function(err, result, pagination, remaining, limit) {
+      console.log(result)
+    });
+  });
 }
+
+//Search route. Uses query parameters to pass in values into the Factual search.
+exports.searchFunction = function(req, res) {
+  console.log("SEARCH1")
+  console.log(req.body)
+  //latitude
+  var latitude = req.body.lat
+  console.log("New Query ----------" );
+  console.log("Latitude " + latitude)
+  //longitude
+  var longitude = req.body.long
+  console.log("Longitude " + longitude)
+  var category = req.body.category;
+  var location = req.body.location;
+  //locality? country?
+  // categories
+  var cat_array = category;
+  console.log(cat_array)
+  var category_ids = new Array();
+  for (var i = 0; i < cat_array.length; i++){
+    console.log(cat_array[i])
+    if (cat_array[i] == "auto"){
+      category_ids.push(2);
+    }
+    else if (cat_array[i] == "gov"){
+      category_ids.push(20);
+    }
+    else if (cat_array[i] == "health"){
+      category_ids.push(62);
+    }
+    else if (cat_array[i] == "landmarks"){
+      category_ids.push(107);
+    }
+    else if (cat_array[i] == "retail"){
+      category_ids.push(123);
+    }
+    else if (cat_array[i] == "biz"){
+      category_ids.push(177);
+    }
+    else if (cat_array[i] == "social"){
+      category_ids.push(308);
+    }
+    else if (cat_array[i] == "sports"){
+      category_ids.push(372);
+    }
+    else if (cat_array[i] == "transport"){
+      category_ids.push(415);
+    }
+    else if (cat_array[i] == "travel"){
+      category_ids.push(430);
+    }
+  }
+  console.log(category_ids);
+  //Note that latitude and longitude must have 6 digits.
+  factual.get('/t/places-us', {filters:{category_ids:{"$includes_any":category_ids}}, geo:{"$circle":{"$center":[+latitude, +longitude],"$meters":1000}}}, function(fact_req, fact_res){
+    // console.log(typeof(res.data));
+    // console.log("length: " + res.data.length);
+    // console.log(res.data[0]);
+    // console.log(typeof(res.data[0]));
+    for (var i = 0; i < fact_res.data.length; i++){
+      var obj = fact_res.data[i];
+      console.log(obj["name"]);
+      console.log(obj["latitude"]);
+      console.log(obj["longitude"]);
+    }
+  });
+  location;
+  searchTerm = category;
+  category = category;
+  latlong = latitude+","+longitude;
+  sortType = 0; // best match
+  numResults = 10;
+  yelp.search({location: location, category_filter: category, cll: latlong, sort: sortType, limit: numResults}, function(error, data) {
+    console.log(data);
+  });
+    res.send('done');
+};
 
 //get followers
 exports.getUsers = function(req,res){
@@ -76,12 +148,14 @@ exports.getUsers = function(req,res){
 		console.log(err)
 	});
 }
+
 // This is where you would initially send users to authorize 
 app.get('/authorize_user', exports.authorize_user);
 // This is your redirect URI 
 app.get('/handleauth', exports.handleauth);
 app.get('/instaSearch', exports.test);
 app.get('/users', exports.getUsers);
+app.post('/search', exports.searchFunction);
 
 app.get('/map', function(req, res) {
   res.render('map', {
@@ -92,76 +166,4 @@ app.get('/map', function(req, res) {
 app.get('/', function(req, res){
 	console.log('request received on index')
 	res.send('homepage');
-});
-
-//Search route. Uses query parameters to pass in values into the Factual search.
-app.get('/search', function(req, res) {
-  //latitude
-  var latitude = req.query.lat
-  console.log("New Query ----------" );
-  console.log("Latitude " + latitude)
-
-  //longitude
-  var longitude = req.query.lon
-  console.log("Longitude " + longitude)
-
-  // categories
-  var cat_array = req.query.categories.split(',');
-  var category_ids = new Array();
-
-  for (var i = 0; i < cat_array.length; i++){
-  	if (cat_array[i] == "auto"){
-  		category_ids.push(2);
-  	}
-  	else if (cat_array[i] == "gov"){
-  		category_ids.push(20);
-  	}
-  	else if (cat_array[i] == "health"){
-  		category_ids.push(62);
-  	}
-  	else if (cat_array[i] == "landmark"){
-  		category_ids.push(107);
-  	}
-  	else if (cat_array[i] == "retail"){
-  		category_ids.push(123);
-  	}
-  	else if (cat_array[i] == "biz"){
-  		category_ids.push(177);
-  	}
-  	else if (cat_array[i] == "social"){
-  		category_ids.push(308);
-  	}
-  	else if (cat_array[i] == "sports"){
-  		category_ids.push(372);
-  	}
-  	else if (cat_array[i] == "transport"){
-  		category_ids.push(415);
-  	}
-  	else if (cat_array[i] == "travel"){
-  		category_ids.push(430);
-  	}
-  }
-
-  // console.log(category_ids);
-
-  location = "Seattle"; // this should be changed to be the user input location!
-  searchTerm = "landmarks";
-  category = "landmarks";
-  latlong = latitude+","+longitude;
-  sortType = 0; // best match
-  numResults = 10;
-  yelp.search({location: location, category_filter: category, cll: latlong, sort: sortType, limit: numResults}, function(error, data) {
-    console.log(data);
-  });
-  	res.send('done');
-
-  var marker_data = null;
-  factual.get('/t/places-us', {filters:{category_ids:{"$includes_any":category_ids}}, geo:{"$circle":{"$center":[+latitude, +longitude],"$meters":1000}}}, function(fact_req, fact_res){
-    marker_data = fact_res.data;
-    
-    res.render('map', {
-      data: marker_data
-    });
-  })
-
 });
