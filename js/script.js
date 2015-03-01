ca5d1b64cfc7c22ce02e2ea652dccc3b3113c828
William Xie <wxie6@illinois.edu>
2015-03-01 04:34:57 -0600

    Newest files

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-// YOUR JAVASCRIPT HERE
 
-function success(position) {
-  var latitude  = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  console.log(position.coords.latitude)
-  console.log(position.coords.longitude)
-};
+/*
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    console.log(position.coords.latitude)
+    console.log(position.coords.longitude)
+  };
 
 function error() {
 	console.log("error")
 };
 
 
+  function error() {
+  	console.log("error")
+  };
+  navigator.geolocation.getCurrentPosition(success, error);*/
