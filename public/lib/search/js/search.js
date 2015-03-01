5550762339453d32edbe7e2aaf98bcbc848d9a19
chrischris292 <cbchan2@illinois.edu>
2015-02-28 22:59:20 -0600

    search

--- public/lib/search/js/search.js
+++ public/lib/search/js/search.js
@@ -1,4 +1,4 @@
-
+var autocomplete; //make it global across files. 
 function setUpAutoComplete(){
  var countryRestrict = { 'country':$("#q1").val()};
 
@@ -10,5 +10,36 @@ function setUpAutoComplete(){
         componentRestrictions: countryRestrict
       });
       autocomplete.setComponentRestrictions({ 'country': $("#q1").val() });
-
 } 
+
+function sendPostData(location){
+    locationObj
+      var locationObj = {"lat": location.k, "long":location.D, "category": ["landmarks"], "location" : $("#q2").val()};
+      locationObj = JSON.stringify(locationObj)
+      console.log(locationObj)
+
+      $.ajax({
+          type: "POST",
+          url: "/search",
+          data: locationObj, 
+          dataType: "json",
+          contentType: "application/json",
+          success: function(msg) {
+              console.log(msg)
+          }
+      });
+      var loc = {"lat": location.k, "long":location.D};
+        loc = JSON.stringify(loc)
+
+      $.ajax({
+          type: "POST",
+          url: "/instaSearch",
+          data: loc, 
+          dataType: "json",
+          contentType: "application/json",
+          success: function(msg) {
+              console.log(msg)
+          }
+      });
+
+}
